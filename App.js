78890627efc5236ae04d8dc55f8197ecc5f041e6
wilson20010327 <wilson.l20010327@gmail.com>
2022-08-29 18:07:04 +0800

    project:creat navigation

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack=createStackNavigator();
+
+const Home = ({navigation}) => { //page 1
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button title='Change to page' onPress={() => navigation.push("Page")}/>
+    </View>
+  );
+};
+const Page = ({navigation}) => { //page 2
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Page Screen</Text>
+      <Button title='Change to Home' onPress={() => navigation.pop()}/>
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer /*Use to pack all the pages which you need to navigate, only one in a project*/> 
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false } } /*the specific page, give it a name and can easily to change between pages *//> 
+        <Stack.Screen name="Page" component={Page} options={{ headerShown: false } } /*the specific page, give it a name and can easily to change between pages *//> 
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
